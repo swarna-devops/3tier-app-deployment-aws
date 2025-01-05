664dbad8502379fc00bf499cfb455a579892e1a9
ReyazShaik <45187103+ReyazShaik@users.noreply.github.com>
2025-01-05 22:56:35 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.cdbmlufgqkjd.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root123456',
+    DB_DATABASE : 'webappdb'
+});
